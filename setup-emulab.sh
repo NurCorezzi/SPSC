5fd4fdc5eb2bc7c0db4677cdc970a7cbc919d0c3
Gabriel Patrick Alcantara Mourao <mourao21@main.mourao21-qv93386.freestore.emulab.net>
2021-03-07 10:20:42 -0700

    setup emulab ok

--- setup-emulab.sh
+++ setup-emulab.sh
@@ -2,11 +2,11 @@
 sudo apt-get update
 sudo apt-get -y install openjdk-8-jdk
 sudo apt-get -y install maven
-export JAVA_HOME /usr
+export JAVA_HOME=/usr
 echo "############## JAVA ################\n\n"
 java -version
 echo "############## MAVEN ################\n\n"
 mvn --version
 echo "############## JAVA_HOME ################\n\n"
-where java
+type -a java
 echo $JAVA_HOME
